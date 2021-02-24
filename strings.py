d51af920de5d46212147ac49b10b633f4a8ede58
Rion H <sciencefixion@pm.me>
2021-02-24 16:07:59 -0700

    Add strings.py

---
+++ strings.py
@@ -0,0 +1,22 @@
+print("Today is a good day to learn Python")
+print('Python is fun')
+print("Python's strings are easy to use")
+print('We can even include "quotes" in strings')
+print("hello" + " world")
+greeting = "Hello"
+name = input("Please enter your name ")
+print(greeting + name)
+
+# if we want a space we can add that too
+print(greeting + ' ' + name)
+
+age = 24
+print(age)
+
+print(type(greeting))
+print(type(age))
+
+age_in_words = "2 years"
+# print(name + " is " + age + " years old")
+# Throws an error which shows Python is a strongly typed language
+print(type(age))
