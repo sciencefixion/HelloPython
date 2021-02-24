d05d798db5bc1b31df55fc7d13847178998cb782
Rion H <sciencefixion@pm.me>
2021-02-24 16:08:50 -0700

    Add strings2.py

---
+++ strings2.py
@@ -0,0 +1,73 @@
+# index positions:
+#         012345678901234
+parrot = "Norwegian Blue"
+
+print(parrot)
+
+print(parrot[3])
+
+#Strings in Python are zero indexed
+#look up sequence data types
+
+# MINI CHALLENGE: Add some code to the program so that it prints out "we win".
+# Each character should get the characters from the parrot string, using indexing.
+# The "w" is already printed out, so you just need to print the remaining 5 characters.
+# With the text that is already being printed, the final output from the program should be:
+
+# Norwegian Blue
+# w
+# e
+#
+# w
+# i
+# n
+
+print(parrot[4])
+print(parrot[9])
+print(parrot[3])
+print(parrot[6])
+print(parrot[8])
+
+print()
+print(parrot[-1])
+print(parrot[-14])
+
+# Mini mini Challenge: print the same output as the above challenge using negative indexing
+
+print()
+print(parrot[-11])
+print(parrot[-1])
+print(parrot[-5])
+print(parrot[-11])
+print(parrot[-8])
+print(parrot[-6])
+
+# negative indexes can be obtained by subracting the total length of the string from the positive index
+print()
+print(parrot[3 - 14])
+print(parrot[4 - 14])
+print(parrot[9 - 14])
+print(parrot[3 - 14])
+print(parrot[6 - 14])
+print(parrot[8 - 14])
+
+# Slicing
+
+print(parrot[0:6]) # Norweg
+# from index 0 up to but not including index 6
+print(parrot[3:5])
+print(parrot[0:9])
+# start value defaults to the start of the sequence
+print(parrot[:9])
+# index 14 is the stop value, the stop value is the default if no end index is specified
+print(parrot[10:14])
+print(parrot[10:])
+
+print(parrot[:6])
+print(parrot[6:])
+
+print(parrot[:6] + parrot[6:])
+
+print(parrot[:]
+
+letters = "abcdefghijklmnopqrstuvwxyz"
