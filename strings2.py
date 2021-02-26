986659ac8be4b16a0ec674258dbf4f6dd1e24644
Rion H <sciencefixion@pm.me>
2021-02-25 21:52:37 -0700

    Update strings2

--- strings2.py
+++ strings2.py
@@ -6,8 +6,8 @@ print(parrot)
 
 print(parrot[3])
 
-#Strings in Python are zero indexed
-#look up sequence data types
+# Strings in Python are zero indexed
+# look up sequence data types
 
 # MINI CHALLENGE: Add some code to the program so that it prints out "we win".
 # Each character should get the characters from the parrot string, using indexing.
@@ -42,7 +42,7 @@ print(parrot[-11])
 print(parrot[-8])
 print(parrot[-6])
 
-# negative indexes can be obtained by subracting the total length of the string from the positive index
+# negative indexes can be obtained by subtracting the total length of the string from the positive index
 print()
 print(parrot[3 - 14])
 print(parrot[4 - 14])
@@ -53,7 +53,7 @@ print(parrot[8 - 14])
 
 # Slicing
 
-print(parrot[0:6]) # Norweg
+print(parrot[0:6])  # Norweg
 # from index 0 up to but not including index 6
 print(parrot[3:5])
 print(parrot[0:9])
@@ -68,6 +68,24 @@ print(parrot[6:])
 
 print(parrot[:6] + parrot[6:])
 
-print(parrot[:]
+print(parrot[:])
 
 letters = "abcdefghijklmnopqrstuvwxyz"
+
+# Slicing with negative numbers
+print(parrot[-14:-8])
+print(parrot[-4:-2])    # Bl
+print(parrot[-4:12])    # Bl
+
+# Using a step in a slice
+print(parrot[0:6:2])    # Nre
+print(parrot[0:6:3])    # Nw
+
+number = "9,223,372,036,854,775,807"
+print(number[1::4])
+number = "9,223;372:036 854,775;807"
+seperators = number[1::4]
+print(seperators)
+
+values = "".join(char if char not in seperators else " " for char in number).split()
+print([int(val) for val in values])
