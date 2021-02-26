799060714b65b5df1a8ebe569fe97aee63af6391
Rion H <sciencefixion@pm.me>
2021-02-26 16:37:13 -0700

    Add interpolation.py

---
+++ interpolation.py
@@ -0,0 +1,11 @@
+# This is depreciated but was used in python 2
+age = 24
+print("My age is %d years" % age)
+
+major = "years"
+minor = "months"
+print("My age is %d %s, %d %s" % (age, major, 6, minor))
+print("Pi is approximately %f" % (22 / 7))
+print("Pi is approximately %60.50f" % (22 / 7))
+
+# x - o - e - String formatting Operations in python 2 doc has more info
