d431955c509694dfe7238d7354c8168445133c4c
Rion Hutsell <st.gish+gh@pm.me>
2021-03-20 19:58:07 -0600

    Add ranges.py

---
+++ ProgramFlow/ranges.py
@@ -0,0 +1,13 @@
+for i in range(1, 21):
+    print("i is now {}".format(i))
+
+for i in range(10):
+    print("i is now {}".format(i))
+
+# steps
+for i in range(0, 10, 2):
+    print("i is now {}".format(i))
+
+for i in range(10, 0, -2):
+    print("i is now {}".format(i))
+
