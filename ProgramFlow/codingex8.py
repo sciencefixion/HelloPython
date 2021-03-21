e83c1feed9f0353402dead2f652bb90b261bf614
Rion Hutsell <st.gish+gh@pm.me>
2021-03-20 19:58:38 -0600

    Add coding exercise 8

---
+++ ProgramFlow/codingex8.py
@@ -0,0 +1,8 @@
+# Write a program to print out all the numbers from 0 to 9
+# Each number should print on a new line (which is the default behavior of print)
+
+for i in range(0, 10):
+    print(i)
+
+for i in range(10):
+    print(i)
