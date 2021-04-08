d25e2359b59d2b27658992ebd2e2378b8851a1c1
Rion H <sciencefixion@pm.me>
2021-04-07 18:03:23 -0600

    Update hilo.py

--- ProgramFlow/hilo.py
+++ ProgramFlow/hilo.py
@@ -6,6 +6,7 @@ input("Press ENTER to start.")
 
 guesses = 1
 while True:
+    print("\tGuessing in the range of {} to {}".format(low, high))
     guess = low + (high - low) // 2  # formula for binary chop
     high_low = input("My guess is {}. Should I guess higher or lower? "
                      "Enter h or l, or c if my guess was correct. "
